0b665036a0238ae59eae26f0bf5d9741eef6b096
Phuchino <34382914+Phuchino@users.noreply.github.com>
2020-05-01 14:48:20 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,10 @@ const ball = Bodies.circle(420, 15, 20)
 
 const boxA = Bodies.rectangle(400, 200, 80, 80)
 const boxB = Bodies.rectangle(450, 50, 80, 80)
+const VulinaGlava = Bodies.circle(550, 500, 200)
 const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true })
 
-World.add(engine.world, [ball, boxA, boxB, ground])
+World.add(engine.world, [ball, boxA, boxB, VulinaGlava, ground])
 
 Engine.run(engine)
 Render.run(render)
